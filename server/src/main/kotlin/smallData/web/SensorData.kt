4ccafda2435ac36c1646be3cf1d35525a5dbb859
patrick.dc <p.del.conte@hotmail.ch>
2016-09-17 21:35:29 +0200

    more sensors

--- server/src/main/kotlin/smallData/web/SensorData.kt
+++ server/src/main/kotlin/smallData/web/SensorData.kt
@@ -2,6 +2,7 @@ package smallData.web
 
 import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Component
+import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.context.WebApplicationContext
 import java.time.LocalDateTime
@@ -16,22 +17,35 @@ import javax.annotation.PostConstruct
 @Scope(WebApplicationContext.SCOPE_APPLICATION)
 open class SensorManager {
 
-    enum class SensorType(val description: String, val stateOnIcon: String, val stateOffIcon: String) {
-        BRIGHTNESS("brightness", "lightbulb-outline", "lightbulb"),
-        MOVEMENT("movement", "walk", "human-male")
+    enum class SensorType(val description: String, val stateOnIcon: String, val stateOffIcon: String, stateOnColor: String, stateOffColor: String) {
+        BRIGHTNESS("brightness", "lightbulb", "lightbulb", "yellow", "black"),
+        MOVEMENT("movement", "walk", "human-male", "blue", "black"),
+        FIREDETECTOR("fire detector", "fire", "fire", "red", "black"),
+        WATERTEMP("water temperature", "oil-temperature", "oil-temperature", "red", "black"),
+        DOOR("door open", "lock-open", "lock", "yellow", "black"),
+        WINDOW("window open", "lock-open", "lock", "yellow", "black")
     }
 
-    data class Sensor(val location: String, val type: SensorType, val dataHistory: MutableList<SensorData>)
+    data class Sensor(val location: String, val type: SensorType, val dataHistory: MutableList<SensorData> = mutableListOf())
 
     open class SensorData(val value: Boolean, val timestamp: LocalDateTime)
     class TimestampedSensorData(val value: Boolean, val timestamp: LocalDateTime)
 
-    val bedroomBrightness = Sensor("Bedroom", SensorType.BRIGHTNESS, mutableListOf())
-    val bedroomMovement = Sensor("Bedroom", SensorType.MOVEMENT, mutableListOf())
-    val livingroomBrightness = Sensor("Living room", SensorType.BRIGHTNESS, mutableListOf())
-    val livingroomMovement = Sensor("Living room", SensorType.MOVEMENT, mutableListOf())
+    //dummy data helpers
+    val bedroomBrightness = Sensor("Bedroom", SensorType.BRIGHTNESS)
+    val bedroomMovement = Sensor("Bedroom", SensorType.MOVEMENT)
+    val livingroomBrightness = Sensor("Living room", SensorType.BRIGHTNESS)
+    val livingroomMovement = Sensor("Living room", SensorType.MOVEMENT)
+    val livingroomWindow = Sensor("Living room", SensorType.WINDOW)
+    val kitchenWaterTemp = Sensor("Kitchen", SensorType.WATERTEMP)
+    val kitchenMovement = Sensor("Kitchen", SensorType.MOVEMENT)
+    val kitchenRefrigator = Sensor("Kitchen", SensorType.WINDOW)
+    val frontDoor = Sensor("Front door", SensorType.DOOR)
+    val balconyDoor = Sensor("Balcony door", SensorType.DOOR)
+    val balconyWindow = Sensor("Balcony window", SensorType.WINDOW)
 
     val sensordata = ArrayList<Sensor>()
+    val sensordataMap = HashMap<SensorType, MutableList<Sensor>>()
 
     @PostConstruct
     fun initializeWithDummyData() {
@@ -44,10 +58,26 @@ open class SensorManager {
             bedroomMovement.dataHistory.add(SensorData(false, LocalDateTime.now().minusDays(rng.nextInt(255).toLong()).minusHours(rng.nextInt(255).toLong())))
             livingroomBrightness.dataHistory.add(SensorData(false, LocalDateTime.now().minusDays(rng.nextInt(255).toLong()).minusHours(rng.nextInt(255).toLong())))
             livingroomMovement.dataHistory.add(SensorData(false, LocalDateTime.now().minusDays(rng.nextInt(255).toLong()).minusHours(rng.nextInt(255).toLong())))
+        }
+
+        sensordataMap.put(SensorType.MOVEMENT, mutableListOf(livingroomMovement, kitchenMovement, bedroomMovement))
+        sensordataMap.put(SensorType.BRIGHTNESS, mutableListOf(bedroomBrightness, livingroomBrightness))
+        sensordataMap.put(SensorType.DOOR, mutableListOf(frontDoor, balconyDoor, kitchenRefrigator))
+        sensordataMap.put(SensorType.WINDOW, mutableListOf(livingroomWindow, balconyWindow))
+
+        sensordataMap.values.forEach { sensordata.addAll(it) }
+    }
+
+    @PostMapping("/register")
+    fun registerSensor(sensortype: String, location: String, id: Int) {
+        when (sensortype) {
+            "brigthness" -> sensordata.add(Sensor(location, SensorType.BRIGHTNESS, mutableListOf()))
+        }
+
+        @PostMapping("data")
+        fun putSensorData(id: Int, value: Boolean) {
 
         }
-        sensordata.addAll(listOf(bedroomBrightness, bedroomMovement, livingroomBrightness, livingroomMovement))
-
     }
 
 
