7e617e4ef1e61a83019188325b2cf8e66b90cf4f
Alexander Kosenkov <alexander@kosenkov.com>
2016-09-17 20:36:25 +0200

    Refactoring of SensorManager

--- server/src/main/kotlin/smallData/web/SensorData.kt
+++ server/src/main/kotlin/smallData/web/SensorData.kt
@@ -1,61 +1,54 @@
 package smallData.web
 
+import org.springframework.context.annotation.Scope
 import org.springframework.stereotype.Component
-import org.springframework.web.bind.annotation.PutMapping
 import org.springframework.web.bind.annotation.RequestMapping
-import java.lang.reflect.Type
-import java.security.Timestamp
-import java.time.Instant
-import java.time.LocalDate
+import org.springframework.web.context.WebApplicationContext
 import java.time.LocalDateTime
 import java.util.*
-import kotlin.reflect.KClass
-import kotlin.reflect.KType
+import javax.annotation.PostConstruct
 
 /**
  * Created by P on 17.09.2016.
  */
 @Component
 @RequestMapping("/sensor")
-class SensorManager{
+@Scope(WebApplicationContext.SCOPE_APPLICATION)
+open class SensorManager {
 
-    enum class SensorType(val description: String ,val stateOnIcon : String, val stateOffIcon : String)  {
-        BRIGHTNESS("brightness","lightbulb-outline","lightbulb"),
-        MOVEMENT("movement","walk","human-male")
+    enum class SensorType(val description: String, val stateOnIcon: String, val stateOffIcon: String) {
+        BRIGHTNESS("brightness", "lightbulb-outline", "lightbulb"),
+        MOVEMENT("movement", "walk", "human-male")
     }
 
-    data class Sensor(val location : String,val type : SensorType,val dataHistory : MutableList<SensorData>)
+    data class Sensor(val location: String, val type: SensorType, val dataHistory: MutableList<SensorData>)
 
-    open class SensorData(val value : Boolean,val timestamp : LocalDateTime)
+    open class SensorData(val value: Boolean, val timestamp: LocalDateTime)
     class TimestampedSensorData(val value: Boolean, val timestamp: LocalDateTime)
 
-    val bedroomBrightness = Sensor("bedroom",SensorType.BRIGHTNESS, mutableListOf())
-    val bedroomMovement = Sensor("bedroom",SensorType.MOVEMENT, mutableListOf())
-    val livingroomBrightness = Sensor("livingroom",SensorType.BRIGHTNESS, mutableListOf())
-    val livingroomMovement = Sensor("livingroom",SensorType.MOVEMENT, mutableListOf())
+    val bedroomBrightness = Sensor("Bedroom", SensorType.BRIGHTNESS, mutableListOf())
+    val bedroomMovement = Sensor("Bedroom", SensorType.MOVEMENT, mutableListOf())
+    val livingroomBrightness = Sensor("Living room", SensorType.BRIGHTNESS, mutableListOf())
+    val livingroomMovement = Sensor("Living room", SensorType.MOVEMENT, mutableListOf())
 
     val sensordata = ArrayList<Sensor>()
 
-    fun initializeWithDummyData(){
+    @PostConstruct
+    fun initializeWithDummyData() {
         val rng = Random(1337)
 
         var nValues = 20
 
-        for (i in 0..nValues){
-            bedroomBrightness.dataHistory.add(SensorData(false, LocalDateTime.now().minusDays( rng.nextInt(255).toLong()).minusHours(rng.nextInt(255).toLong())))
-            bedroomMovement.dataHistory.add(SensorData(false, LocalDateTime.now().minusDays( rng.nextInt(255).toLong()).minusHours(rng.nextInt(255).toLong())))
-            livingroomBrightness.dataHistory.add(SensorData(false, LocalDateTime.now().minusDays( rng.nextInt(255).toLong()).minusHours(rng.nextInt(255).toLong())))
-            livingroomMovement.dataHistory.add(SensorData(false, LocalDateTime.now().minusDays( rng.nextInt(255).toLong()).minusHours(rng.nextInt(255).toLong())))
+        for (i in 0..nValues) {
+            bedroomBrightness.dataHistory.add(SensorData(false, LocalDateTime.now().minusDays(rng.nextInt(255).toLong()).minusHours(rng.nextInt(255).toLong())))
+            bedroomMovement.dataHistory.add(SensorData(false, LocalDateTime.now().minusDays(rng.nextInt(255).toLong()).minusHours(rng.nextInt(255).toLong())))
+            livingroomBrightness.dataHistory.add(SensorData(false, LocalDateTime.now().minusDays(rng.nextInt(255).toLong()).minusHours(rng.nextInt(255).toLong())))
+            livingroomMovement.dataHistory.add(SensorData(false, LocalDateTime.now().minusDays(rng.nextInt(255).toLong()).minusHours(rng.nextInt(255).toLong())))
 
         }
-        sensordata.addAll(listOf(bedroomBrightness,bedroomMovement,livingroomBrightness,livingroomMovement))
+        sensordata.addAll(listOf(bedroomBrightness, bedroomMovement, livingroomBrightness, livingroomMovement))
 
     }
-
-
-
-
-
 
 
 }
